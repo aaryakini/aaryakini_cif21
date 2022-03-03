b753ee40ed1427dcbbd2bed9d6f9d4d329436a08
aaryakini <kinia313@newschool.edu>
2022-03-03 13:53:50 -0500

    changing toggle for rock garden sort

--- projects/rockGarden/index2.js
+++ projects/rockGarden/index2.js
@@ -1,17 +1,7 @@
 let body = document.body;
 
+//shows only plastic
 function togglePlastic(){
-	let x = document.getElementsByClassName("plastic");
-	for (let i = 0; i < x.length; i++){
-		if (x[i].style.display === "block") {
-		x[i].style.display = "none";
-		} else {
-			x[i].style.display = "block";
-		}
-	}
-}
-
-function toggleNonPlastic(){
 	let x = document.getElementsByClassName("nonplastic");
 	for (let i = 0; i < x.length; i++){
 		if (x[i].style.display === "block") {
@@ -22,8 +12,9 @@ function toggleNonPlastic(){
 	}
 }
 
-function toggleRecycled(){
-	let x = document.getElementsByClassName("recycled");
+//shows only nonplastic
+function toggleNonPlastic(){
+	let x = document.getElementsByClassName("plastic");
 	for (let i = 0; i < x.length; i++){
 		if (x[i].style.display === "block") {
 		x[i].style.display = "none";
@@ -33,8 +24,21 @@ function toggleRecycled(){
 	}
 }
 
-function toggleNonRecycled(){
+//shows only recycled
+function toggleRecycled(){
 	let x = document.getElementsByClassName("nonrecycled");
+	for (let i = 0; i < x.length; i++){
+		if (x[i].style.display === "block") {
+		x[i].style.display = "none";
+		} else {
+			x[i].style.display = "block";
+		}
+	}
+}
+
+//shows only nonrecycled
+function toggleNonRecycled(){
+	let x = document.getElementsByClassName("recycled");
 	for (let i = 0; i < x.length; i++){
 		if (x[i].style.display === "block") {
 		x[i].style.display = "none";

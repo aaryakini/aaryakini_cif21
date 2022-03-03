052496dc36e08aaf99b105825df8d4124d038845
aaryakini <kinia313@newschool.edu>
2022-03-03 13:44:13 -0500

    Editing Homepage (index) Javascript and CSS

--- index1.js
+++ index1.js
@@ -6,41 +6,85 @@
 ***************************************************************************************/
 
 var draggableElements = document.getElementsByClassName("draggable");
+console.log(draggableElements.length); //checking that array exists and is populated
 
 for(var i = 0; i < draggableElements.length; i++){
     dragElement(draggableElements[i]);
 }
 
-function dragElement(elmnt) {
+function dragElement(element) {
     var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-    if (document.getElementById(elmnt.id + "header")) {
-        document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-    } else {
-        elmnt.onmousedown = dragMouseDown;
-    }
+    element.onmousedown = dragMouseDown;
+
     function dragMouseDown(e) {
         e = e || window.event;
-        pos3 = parseInt(e.clientX);
-        pos4 = parseInt(e.clientY);
+        e.preventDefault();
+        // get the mouse cursor position at startup:
+        pos3 = e.clientX;
+        pos4 = e.clientY;
         document.onmouseup = closeDragElement;
+        // call a function whenever the cursor moves:
         document.onmousemove = elementDrag;
-        return false;
     }
 
     function elementDrag(e) {
         e = e || window.event;
-        pos1 = pos3 - parseInt(e.clientX);
-        pos2 = pos4 - parseInt(e.clientY);
-        pos3 = parseInt(e.clientX);
-        pos4 = parseInt(e.clientY);
-        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-        console.log(elmnt.offsetTop)
-        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+        e.preventDefault();
+        // calculate the new cursor position:
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // set the element's new position:
+        element.style.top = (element.offsetTop - pos2) + "px";
+        element.style.left = (element.offsetLeft - pos1) + "px";
     }
 
     function closeDragElement() {
+        // stop moving when mouse button is released:
         document.onmouseup = null;
         document.onmousemove = null;
     }
 }
 /***************************************************************************************/
+
+// function rockGarden(){
+//     console.log("clicked");
+//     let w = window.open("_self");
+//     w.location = "pages/rockGarden.html"; //how to assign the url to the newly opened window
+// }
+
+
+// this is the jQuery stuff, modified to suit my html
+// https://jsfiddle.net/p8dg76LL/1/
+// function dragAndListen(){
+//     console.log("its working!");
+//   $(".draggable").draggable();
+//   $(".draggable i").off("click").on("click", 
+//         function rockGarden(){
+//         console.log("clicked");
+//         let w = window.open("_self");
+//         w.location = "pages/rockGarden.html"; //how to assign the url to the newly opened window
+//         }
+//     );
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

a1e54c4e79255f9d25ed416c08042aa4162d171b
aaryakini <kinia313@newschool.edu>
2022-03-03 14:05:17 -0500

    same thing again ✌🏽😔 fixing the underline on navbar

--- projects/rockGarden/index2.js
+++ projects/rockGarden/index2.js
@@ -3,10 +3,10 @@ let body = document.body;
 //shows only plastic
 function togglePlastic(){
 	let x = document.getElementsByClassName("nonplastic");
+	let j=document.getElementById("plastic");
 	for (let i = 0; i < x.length; i++){
 		if (x[i].style.display === "block") {
 			x[i].style.display = "none";
-			let j=document.getElementById("plastic");
 			j.style.textDecoration = "underline";
 		} else {
 			x[i].style.display = "block";
@@ -19,10 +19,10 @@ function togglePlastic(){
 //shows only nonplastic
 function toggleNonPlastic(){
 	let x = document.getElementsByClassName("plastic");
+	let j=document.getElementById("nonplastic");
 	for (let i = 0; i < x.length; i++){
 		if (x[i].style.display === "block") {
 			x[i].style.display = "none";
-			let j=document.getElementById("nonplastic");
 			j.style.textDecoration = "underline";
 		} else {
 			x[i].style.display = "block";
@@ -34,10 +34,10 @@ function toggleNonPlastic(){
 //shows only recycled
 function toggleRecycled(){
 	let x = document.getElementsByClassName("nonrecycled");
+	let j=document.getElementById("recycled");
 	for (let i = 0; i < x.length; i++){
 		if (x[i].style.display === "block") {
 			x[i].style.display = "none";
-			let j=document.getElementById("recycled");
 			j.style.textDecoration = "underline";
 		} else {
 			x[i].style.display = "block";
@@ -49,10 +49,10 @@ function toggleRecycled(){
 //shows only nonrecycled
 function toggleNonRecycled(){
 	let x = document.getElementsByClassName("recycled");
+	let j=document.getElementById("nonrecycled");
 	for (let i = 0; i < x.length; i++){
 			if (x[i].style.display === "block") {
 			x[i].style.display = "none";
-			let j=document.getElementById("nonrecycled");
 			j.style.textDecoration = "underline";
 		} else {
 			x[i].style.display = "block";

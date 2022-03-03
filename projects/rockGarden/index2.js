525b536103e6fc082bd7aa0907ffaaaca3fb14e5
aaryakini <kinia313@newschool.edu>
2022-03-03 14:03:07 -0500

    fixing underline for navbar

--- projects/rockGarden/index2.js
+++ projects/rockGarden/index2.js
@@ -5,11 +5,13 @@ function togglePlastic(){
 	let x = document.getElementsByClassName("nonplastic");
 	for (let i = 0; i < x.length; i++){
 		if (x[i].style.display === "block") {
-		x[i].style.display = "none";
-		let j=document.getElementById("plastic");
-		j.style.textDecoration = "underline";
+			x[i].style.display = "none";
+			let j=document.getElementById("plastic");
+			j.style.textDecoration = "underline";
 		} else {
 			x[i].style.display = "block";
+			j.style.textDecoration = "none";
+
 		}
 	}
 }
@@ -19,11 +21,12 @@ function toggleNonPlastic(){
 	let x = document.getElementsByClassName("plastic");
 	for (let i = 0; i < x.length; i++){
 		if (x[i].style.display === "block") {
-		x[i].style.display = "none";
-		let j=document.getElementById("nonplastic");
-		j.style.textDecoration = "underline";
+			x[i].style.display = "none";
+			let j=document.getElementById("nonplastic");
+			j.style.textDecoration = "underline";
 		} else {
 			x[i].style.display = "block";
+			j.style.textDecoration = "none";
 		}
 	}
 }
@@ -33,11 +36,12 @@ function toggleRecycled(){
 	let x = document.getElementsByClassName("nonrecycled");
 	for (let i = 0; i < x.length; i++){
 		if (x[i].style.display === "block") {
-		x[i].style.display = "none";
-		let j=document.getElementById("recycled");
-		j.style.textDecoration = "underline";
+			x[i].style.display = "none";
+			let j=document.getElementById("recycled");
+			j.style.textDecoration = "underline";
 		} else {
 			x[i].style.display = "block";
+			j.style.textDecoration = "none";
 		}
 	}
 }
@@ -46,12 +50,13 @@ function toggleRecycled(){
 function toggleNonRecycled(){
 	let x = document.getElementsByClassName("recycled");
 	for (let i = 0; i < x.length; i++){
-		if (x[i].style.display === "block") {
-		x[i].style.display = "none";
-		let j=document.getElementById("nonrecycled");
-		j.style.textDecoration = "underline";
+			if (x[i].style.display === "block") {
+			x[i].style.display = "none";
+			let j=document.getElementById("nonrecycled");
+			j.style.textDecoration = "underline";
 		} else {
 			x[i].style.display = "block";
+			j.style.textDecoration = "none";
 		}
 	}
 }
